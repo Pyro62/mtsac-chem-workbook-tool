1cdc488739e769281296959ae5aba4ce7f2ae602
Ryan Yeung <ryeung0305@gmail.com>
2026-08-11 10:44:31 -0700

    implmented file validation for test file

--- backend/processing.py
+++ backend/processing.py
@@ -198,15 +198,16 @@ def process_assessment(test_df, student_info_df = None):
 
     return result
 
-student_info_df = pd.read_excel('../test_data/classList.xls', header = None)
+#student_info_df = pd.read_excel('../test_data/classList.xls', header = None)
 test_df = pd.read_excel('../test_data/newAssessment.xlsx')
+print("QuizClass" in test_df.columns)
 
-result = process_assessment(test_df, student_info_df)
+#result = process_assessment(test_df, student_info_df)
 
-for student in result:
+'''for student in result:
     print(f"Student ID: {student}")
     print(f"Name: {result[student]['name']}")
     print(f"Score: {result[student]['score']}")
     print(f"Topics to Review: {result[student]['topics_to_review']}")
     print()
-
+'''
